5e3fc24af54b921f1ec89252b9202169c0801465
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-06-06 19:03:55 +0900

    Added about page

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,12 @@
-export default () => <div>Welcome to next.js!</div>
+import Link from 'next/link'
+
+const Index = () => (
+    <div>
+        <Link href="/about">
+            <a>About Page</a>
+        </Link>
+        <p>Hello Next.js</p>
+    </div>
+)
+
+export default Index
